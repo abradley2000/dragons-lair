c9253950f5d58b77b22237e349933b3dd493ac8c
abradley2000 <aaron.s.bradley2000@gmail.com>
2021-01-18 22:21:54 -0600

    completed dragons-lair

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import './Header.css';
 
@@ -29,15 +30,46 @@ export default class Header extends Component {
 
   login() {
     // axios POST to /auth/login here
-  }
+    const {username, password} = this.state;
+    axios.post('/auth/login', {username, password})
+      .then(user => {
+        this.setState({
+          username: '',
+          password: ''
+        })
+        this.props.updateUser(user.data)
+      })
+      .catch(error => {
+        this.setState({
+          username: '',
+          password: ''
+        })
+        alert(error.response.request.response)
+      })
+  };
 
   register() {
     // axios POST to /auth/register here
-  }
+    const {username, password, isAdmin} = this.state;
+    axios.post('/auth/register', {username, password, isAdmin})
+      .then(user => {
+        this.setState({
+          username: '',
+          password: ''})
+        this.props.updateUser(user.data)
+      })
+      .catch(error => {
+        this.setState({username: '', password: ''})
+        alert(error.response.request.response)
+      })
+  };
 
   logout() {
     // axios GET to /auth/logout here
-  }
+    axios.get('/auth/logout')
+      .then(() => this.props.updateUser({}))
+      .catch(error => console.log(error))
+  };
 
   render() {
     const { username, password } = this.state;
